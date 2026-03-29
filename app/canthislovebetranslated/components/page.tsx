a3ca2341b492b57c7ca4ecf5b9d1b878fd52c308
rahscripts <rahmantjis@gmail.com>
2026-03-30 01:27:12 +0530

    basic implentation done

---
+++ app/canthislovebetranslated/components/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function CanThisLoveBeTranslatedNavbar() {
+    return (
+        <div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
---
+++ app/components/WatchNowBtn.tsx
@@ -0,0 +1,7 @@
+export default function WatchNowBtn() {
+    return (
+        <button>
+
+        </button>
+    )
+}
